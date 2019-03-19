9cd94e17c266c9e21bfad779cf9d947f710a3231
weedjy <weedjy@gmail.com>
2019-03-19 22:21:18 +0300

    updates after 6353

--- collatzResultSorter.py
+++ collatzResultSorter.py
@@ -13,18 +13,18 @@ checked_primes_set = set()
 primes_to_check = set()
 cycles = dict()
 for ln in results:
-	words = ln.strip(' \n\r')
-	words = words.split()
-	if words[0] == u'exploring':
-		current_prime = int(words[1])
-		checked_primes_set.add(current_prime)
-		primes_to_check.add(current_prime)
-		cycles[current_prime] = []
-	else:
-		checked_prime = int(words[2])
-		if checked_prime in primes_to_check:
-			primes_to_check.remove(checked_prime)
-		cycles[checked_prime].append(int(words[3]))
+    words = ln.strip(' \n\r')
+    words = words.split()
+    if words[0] == u'exploring':
+        current_prime = int(words[1])
+        checked_primes_set.add(current_prime)
+        primes_to_check.add(current_prime)
+        cycles[current_prime] = []
+    else:
+        checked_prime = int(words[2])
+        if checked_prime in primes_to_check:
+            primes_to_check.remove(checked_prime)
+        cycles[checked_prime].append(int(words[3]))
 
 primes_to_check_file = codecs.open("primes-to-check.txt", "w", "utf-8")
 output = list(primes_to_check)
@@ -36,17 +36,17 @@ primes_to_check_file.close()
 
 sorted_cycles = collections.OrderedDict()
 for key in sorted(cycles.keys()):
-	if len(cycles[key]) > 0:
-		sorted_cycles[key] = sorted(cycles[key])
+    if len(cycles[key]) > 0:
+        sorted_cycles[key] = sorted(cycles[key])
 
 
 for key, val in sorted_cycles.items():
-	print(key, val)
+    print(key, val)
 
 
 x = collections.OrderedDict()
 for key in sorted_cycles.keys():
-	x[key] = len(sorted_cycles[key])
+    x[key] = len(sorted_cycles[key])
 
 
 plt.bar(x.keys(), x.values(), width=25.0, color='b')
@@ -56,7 +56,7 @@ counts = ('found', 'not found')
 x_pos = np.arange(len(counts))
 c_values = [len(x.keys()), 1000 - len(x.keys())]
 
-plt.bar(x_pos, c_values, align='center', alpha=0.5, color = 'r')
+plt.bar(x_pos, c_values, align='center', alpha=0.5, color='r')
 plt.xticks(x_pos, counts)
 plt.ylabel('count')
 plt.title('found vs. not found')
